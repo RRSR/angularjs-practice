3a8e98d2e240e2d71dea14dbf3ee8d922dbf00a0
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-09-04 23:32:40 +0530

    Looping with ngRepeat

--- more-directives/app.js
+++ more-directives/app.js
@@ -1,6 +1,12 @@
 angular.module("moreDirectives", [])
-.controller("MoreDirectivesController", MoreDirectivesController);
+    .controller("MoreDirectivesController", MoreDirectivesController);
 
-function MoreDirectivesController(){
-
+function MoreDirectivesController() {
+    this.myArray = [
+        { "Name": "Foo", "Age": "24" },
+        { "Name": "Bar", "Age": "31" },
+        { "Name": "Boo", "Age": "24" },
+        { "Name": "Buzz", "Age": "26" },
+        { "Name": "Lii", "Age": "21" }
+    ];
 }
