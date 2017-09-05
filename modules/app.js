c11519c6faac0268994ea0314704a282b9c941ba
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-09-05 23:31:10 +0530

    Using an External Module

--- modules/app.js
+++ modules/app.js
@@ -1,7 +1,17 @@
-var modulesApp = angular.module("modulesApp", ["baseModule"]);
+var modulesApp = angular.module('modulesApp', ['baseModule', 'ngTagsInput']);
 
 modulesApp.controller("AppCtrl", AppCtrl);
 
 function AppCtrl() {
     this.message = "This is a message from the app module!";
+    this.tags = [{
+            text: 'Superman'
+        },
+        {
+            text: 'Spiderman'
+        },
+        {
+            text: 'Batman'
+        }
+    ];
 }
